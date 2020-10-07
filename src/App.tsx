2eed989680416b501418a709c1d6ee05b957983f
Malek Smida <msmida96@gmail.com>
2020-10-07 17:57:36 +0100

    #Add: Counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Test from "./components/Test";
+import Counter from "./components/Counter";
 
 function App() {
   return (
@@ -10,6 +11,15 @@ function App() {
         selected={false}
         obj={{ name: "laabidi", email: "laabidi@gmail.com" }}
       />
+
+      <Counter>
+        {({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
     </div>
   );
 }
